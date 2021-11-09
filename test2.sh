666d4a42e725e95621beaf587c2bfdfe64129ff1
GD7190765108 <3294095282@qq.com>
2021-11-09 20:44:33 +0800

    second

---
+++ test2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "my home dir is $Home"
+echo "my working dir is `pwd`"
---
+++ test4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read name animal1 animal2 animal3 animal
+echo "welcom to our ${name}_zoo"
+echo "we have $animal1 $animal2 $animal3"
